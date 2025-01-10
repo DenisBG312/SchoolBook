6a80d57e305e72bc8daf4f490d804794c694cf3f
Denis Nedkov <113765690+DenisBG312@users.noreply.github.com>
2025-01-10 21:48:25 +0200

    made the exam index view

---
+++ SchoolBook.Web.ViewModels/Exam/ExamIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SchoolBook.Web.ViewModels.Exam
+{
+    public class ExamIndexViewModel
+    {
+        public int Id { get; set; }
+        public string SubjectName { get; set; }
+        public string ExamName { get; set; }
+        public DateTime ExamDate { get; set; }
+    }
+}
---
+++ SchoolBook.Web/Controllers/ExamController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SchoolBook.Web.Data;
+using SchoolBook.Web.ViewModels.Exam;
+
+namespace SchoolBook.Web.Controllers
+{
+    public class ExamController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ExamController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var exams = await _context.Exams
+                .Include(s => s.Subject)
+                .Select(e => new ExamIndexViewModel()
+                {
+                    Id = e.Id,
+                    ExamDate = e.ExamDate,
+                    ExamName = e.ExamName,
+                    SubjectName = e.Subject.SubjectName
+                })
+                .ToListAsync();
+
+            return View(exams);
+        }
+    }
+}
