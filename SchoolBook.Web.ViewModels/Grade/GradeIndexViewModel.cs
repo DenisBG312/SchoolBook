7a3e03dcc98ee032f7ddd843853de9ec7ca1df41
Denis Nedkov <113765690+DenisBG312@users.noreply.github.com>
2025-01-11 15:42:27 +0200

    made the grade index view model

---
+++ SchoolBook.Web.ViewModels/Grade/GradeIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolBook.Web.ViewModels.Grade
+{
+    public class GradeIndexViewModel
+    {
+        public string EvaluationName { get; set; }
+        public DateTime GradedDate { get; set; }
+        public string ExamName { get; set; }
+    }
+}
---
+++ SchoolBook.Web/Controllers/GradeController.cs
@@ -0,0 +1,49 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SchoolBook.Web.Data;
+using SchoolBook.Web.ViewModels.Grade;
+
+namespace SchoolBook.Web.Controllers
+{
+    public class GradeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GradeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var currentUserId = GetUserId();
+
+            var student = await _context.Students
+                .FirstOrDefaultAsync(s => s.UserId == currentUserId);
+
+            if (student == null)
+            {
+                return NotFound("You are not registered as a student.");
+            }
+
+            var gradesForCurrentStudent = await _context.Grades
+                .Include(e => e.Exam)
+                .Where(g => g.StudentId == student.Id)
+                .Select(g => new GradeIndexViewModel()
+                {
+                    ExamName = g.Exam.ExamName,
+                    EvaluationName = g.EvaluationName,
+                    GradedDate = g.GradedDate
+                })
+                .ToListAsync();
+
+            return View(gradesForCurrentStudent);
+        }
+
+
+        private string GetUserId()
+        {
+            return User.FindFirstValue(ClaimTypes.NameIdentifier)!;
+        }
+    }
+}
