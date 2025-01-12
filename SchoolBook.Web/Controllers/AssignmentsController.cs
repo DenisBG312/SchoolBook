a6e939a9529efbe5f813ee7c95cb999ba7f001e0
Denis Nedkov <113765690+DenisBG312@users.noreply.github.com>
2025-01-13 00:13:04 +0200

    Added new controller

---
+++ SchoolBook.Web/Controllers/AssignmentsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SchoolBook.Web.Data;
+
+namespace SchoolBook.Web.Controllers
+{
+	public class AssignmentsController : Controller
+	{
+		private ApplicationDbContext _context;
+		public AssignmentsController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			var assignments = await _context.Assignments.ToListAsync();
+
+			return View(assignments);
+		}
+	}
+}
