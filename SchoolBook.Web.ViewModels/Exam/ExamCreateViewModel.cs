8180e4fe77de7283f2d2d94f900aa6fcfbe3bda6
Meral <Meral@DESKTOP-FDH0SAE>
2025-01-15 11:33:46 +0200

    made the view models prop more user-friendly

--- SchoolBook.Web.ViewModels/Exam/ExamCreateViewModel.cs
+++ SchoolBook.Web.ViewModels/Exam/ExamCreateViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -12,18 +13,21 @@ namespace SchoolBook.Web.ViewModels.Exam
     {
         public int Id { get; set; }
         [Required]
+        [DisplayName("Exam Name")]
         [StringLength(100, MinimumLength = 3)]
         public string ExamName { get; set; } = string.Empty;
         [Required]
+        [DisplayName("Exam Date")]
         public DateTime ExamDate { get; set; } = DateTime.Now;
         [Required]
+        [DisplayName("Maximum Marks")]
         public int MaximumMarks { get; set; }
         [Required]
-        [Display(Name = "Subject")]
+        [DisplayName("Subject")]
         public int SubjectId { get; set; }
         public SelectList Subjects { get; set; }
         [Required]
-        [Display(Name = "Classes")]
+        [DisplayName("Classes")]
         public List<int> SelectedClassIds { get; set; } = new List<int>();
         public List<SelectListItem> ClassCheckboxes { get; set; } = new List<SelectListItem>();
     }
